af0748f02693a53f661d3dbdb1e6387a8c64c3b7
luanvo0406 <vominhluan0406@gmail.com>
2021-05-07 22:29:12 +0700

    init source

---
+++ src/main/java/com/luanvo/coincat/CoincatApplication.java
@@ -0,0 +1,13 @@
+package com.luanvo.coincat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoincatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoincatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coincat/CoincatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coincat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoincatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
